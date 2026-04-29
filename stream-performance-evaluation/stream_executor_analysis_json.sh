f15fc8cd28adefc8814fc8f2137ea0a2e81e66db
Luigi Asprino <luigi.asprino@gmail.com>
2026-04-29 07:37:10 +0200

    add script for performance analysis in json

---
+++ stream-performance-evaluation/stream_executor_analysis_json.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#
+# Copyright (c) 2022 SPARQL Anything Contributors @ http://github.com/sparql-anything
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+#./stream_executor_analysis_json.sh /Users/lgu/workspace/SPARQL-Anything/fxbgp/target/fxbgp-test-executor-1.2.0-SNAPS HOT.jar  /Users/lgu/Desktop/ISWC2026-test/input  /Users/lgu/Desktop/ISWC2026-test/stream-performance-json
+
+source functions.sh
+
+JAR=$1
+INPUT_FOLDER=$2
+RESULT_FOLDER=$3
+
+MEM=50
+EXECUTOR="stream"
+SIZE="100000"
+FORMAT="json"
+QTs=("H=2_K=1000" "H=3_K=100" "H=4_K=32" "H=5_K=16" "H=6_K=10")
+
+MAX_TPs=10
+for ((TPs=1; TPs<=MAX_TPs; TPs++)); do
+    inner_limit=$(( TPs * 2 + 1 ))
+    for ((VARs=1; VARs<=inner_limit; VARs++)); do
+      for ((RUN=1; RUN<=3; RUN++)); do
+        for QT in "${QTs[@]}"; do
+
+          UC="${QT} ${TPs} ${VARs} 0 ${SIZE} ${FORMAT}"
+          echo "UC ${UC}"
+          monitor-query-stream ${JAR} ${MEM} ${INPUT_FOLDER} ${UC} ${EXECUTOR} ${RESULT_FOLDER} ${RUN} 5
+
+          UC="${QT} ${TPs} ${VARs} 1 ${SIZE} ${FORMAT}"
+          echo "UC ${UC}"
+          monitor-query-stream ${JAR} ${MEM} ${INPUT_FOLDER} ${UC} ${EXECUTOR} ${RESULT_FOLDER} ${RUN} 5
+
+        done
+      done
+    done
+done
